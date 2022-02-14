57b0fa6eaa915d8c043e7928d6125546edb95ded
Brankond <48344507+Brankond@users.noreply.github.com>
2022-02-14 09:13:25 +0800

    Update ADDSERVER.sh
    Change host name

--- ADDSERVER.sh
+++ ADDSERVER.sh
@@ -1,9 +1,9 @@
 #Registering Hosts
-gpuview add --url http://10.80.42.91:9999 --name DGX-1
-gpuview add --url http://10.80.42.177:9999 --name DGX-2
-gpuview add --url http://10.80.42.93:9999 --name DGX-3
-gpuview add --url http://10.80.43.55:9999 --name DGX-Station-1
-gpuview add --url http://10.80.42.195:9999 --name DGX-Station-2
-gpuview add --url http://10.80.42.229:9999 --name DGX-Station-3
-gpuview add --url http://10.80.42.201:9999 --name LV-10gpu-server-1
-gpuview add --url http://10.80.42.178:9999 --name LV-10gpu-server-2
+gpuview add --url http://10.80.42.91:9999 --name Pascal
+gpuview add --url http://10.80.42.177:9999 --name Volta-1
+gpuview add --url http://10.80.42.93:9999 --name Volta-2
+gpuview add --url http://10.80.43.55:9999 --name Station-1
+gpuview add --url http://10.80.42.195:9999 --name Station-2
+gpuview add --url http://10.80.42.229:9999 --name Station-3
+gpuview add --url http://10.80.42.201:9999 --name Ten-1
+gpuview add --url http://10.80.42.178:9999 --name Ten-2
